36334d0f5c3ff11913302b85222569784b3f82b3
fedejeanne <Federico.Jeanne@vector.com>
2024-09-12 12:49:52 +0200

    Change SnippetDoubleDisplay

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/SnippetDoubleDisplay.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/SnippetDoubleDisplay.java
@@ -1,17 +1,43 @@
 package org.eclipse.swt.snippets;
 
+import org.eclipse.swt.*;
+import org.eclipse.swt.events.*;
+import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
 
 /**
- * Open up a 2nd display in the UI thread.
+ * Open up a 2nd display in a non-UI thread and run its main loop too.
  */
 public class SnippetDoubleDisplay {
 
 	public static void main(String[] args) {
-		new Display();
+		new Thread(() -> doIt()).start();
+		doIt();
+	}
+
+	private static void doIt() {
 		Display display = new Display();
 		Shell shell = new Shell(display);
-		shell.setText("Snippet 1");
+		shell.setText("Thread: '" + Thread.currentThread().getName() + "'");
+		shell.setLayout(new GridLayout());
+
+		Label label = new Label(shell, SWT.WRAP | SWT.BORDER);
+		GridData labelData = new GridData();
+		labelData.widthHint = 10;
+		labelData.horizontalAlignment = SWT.FILL;
+		label.setLayoutData(labelData);
+
+		Button button = new Button(shell, SWT.PUSH);
+		button.setText("Click this button and you'll see the label above change its content");
+		button.addSelectionListener(
+				SelectionListener.widgetSelectedAdapter(e -> label.setText(label.getText() + " CLICK!")));
+
+		shell.pack();
+
+		labelData.widthHint = label.getBounds().width;
+
+		shell.pack();
+
 		shell.open();
 		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch())
@@ -19,4 +45,5 @@ public class SnippetDoubleDisplay {
 		}
 		display.dispose();
 	}
+
 }
